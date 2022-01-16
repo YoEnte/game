d0aae4dd73543afc0ef67ebf1c39f979a17564f4
YoEnte <67060214+YoEnte@users.noreply.github.com>
2022-01-16 15:08:01 +0000

    change

--- pages/.external_file/script.js
+++ pages/.external_file/script.js
@@ -5,5 +5,8 @@ console.log(func_test(3));
 var yo = new Class_test('yoente');
 console.log(yo.my_name());
 
+var tim = new Class_test('tim');
+console.log(tim.my_name());
+
 var ente = new Entity('ente');
 console.log(ente.my_name())
